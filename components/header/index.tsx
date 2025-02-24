175bd51f1e9126f0ea5f7973954ae53266908466
syedazeemjaved <syedazeemjaved@gmail.com>
2025-02-24 20:45:14 +0500

    [Fix] Header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,3 +1,5 @@
+'use server'
+
 import Image from 'next/image'
 
 import MobileMenu from './MobileMenu'
@@ -6,7 +8,7 @@ import type { ReactElement } from 'react'
 
 const Header = (): ReactElement => {
   return (
-    <header className="absolute z-10 w-full bg-black text-white lg:bg-transparent">
+    <header className="absolute z-20 w-full bg-black text-white lg:bg-transparent">
       <div className="flex items-center justify-between p-4 lg:px-32 lg:py-8">
         <Image
           src="/damac-logo-white.webp"
