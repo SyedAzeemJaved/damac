9ff3c0d55a7792f6ac269976e5359c74aa76ed23
syedazeemjaved <syedazeemjaved@gmail.com>
2025-02-25 07:33:56 +0500

    [Fix] Characters escape error

--- components/whats-happening/index.tsx
+++ components/whats-happening/index.tsx
@@ -8,7 +8,7 @@ const WhatsHappening = (): ReactElement => {
   return (
     <div className="mt-24 w-full bg-white text-black">
       <h3 className="font-termina-medium text-center text-2xl lg:text-5xl">
-        What's happening
+        What&apos;s happening
       </h3>
       <h4 className="mt-4 text-center text-lg leading-tight lg:text-xl">
         DAMAC continues to make headlines across globe in different sectors and
