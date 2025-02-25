ec8627aea18401ad5bbc04056332e2355fdb8870
syedazeemjaved <syedazeemjaved@gmail.com>
2025-02-25 07:25:12 +0500

    [Add] Fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,25 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'Arial', 'sans-serif'],
+        termina: ['Termina', 'Arial', 'sans-serif'],
+        'termina-medium': ['Termina-Medium', 'sans-serif'],
+      },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
